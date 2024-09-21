dac89e21ff9f0fdd624a1b517d604486eed6bb2e
lsh9084 <sanghun1101088@gmail.com>
2024-09-21 11:20:32 +0900

    .

--- src/main/java/com/farmstory/config/SecurityConfig.java
+++ src/main/java/com/farmstory/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
+        
         return http.build();
 
     }
